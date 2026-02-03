fdcffde71f41b4a5e9d7ef0d134221e7181bd4b0
rchouhani <rchouhani@me.com>
2026-02-03 18:10:46 +0100

    add/ home structure

--- compowser/src/app/page.js
+++ compowser/src/app/page.js
@@ -1,65 +1,27 @@
-import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="bg-[#171926]" >
+    <Navbar />
+    <div className="bg-[#171926] flex flex-col items-start justify-start mx-14 p-10 font-sans">
+      <h1 className="text-white text-5xl font-semibold mb-5">Copiez, Collez, Codez !</h1>
+      <p className="text-[#777EA8] mb-4 w-120">
+        Ici, tu trouveras des exemples de composants réutilisables, simples et
+        basiques, que tu pourras customiser toi-même dans ton code ! On te donne
+        juste un premier petit coup de pouce, à toi de jouer !
+      </p>
+      <p className="text-[#777EA8] w-120">
+        Tu as une vision sur le visuel, et à côté le code du component à
+        récupérer directement.
+      </p>
+    </div>
+    <section className="flex flex-col items-center">
+      <h2 className="flex text-4xl text-transparent mt-6 [-webkit-text-stroke:1px_rgb(48,237,193,1)]">
+        TROUVEZ COS COMPONENTS
+      </h2>
+
+    </section>
     </div>
   );
 }
