8749222b1d1feb0dbdf972be0e24e315d31111ed
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-02-04 14:48:01 +0100

    edit: add responsive style on navbar component

--- compowser/src/app/components/Navbar.js
+++ compowser/src/app/components/Navbar.js
@@ -2,15 +2,22 @@ import Link from "next/link";
 
 export default function () {
     return (
-        <nav className="flex flex-row justify-between items-end bg-[#171926] p-10 inter">
-            <div className="text-3xl text-transparent [-webkit-text-stroke:1px_rgb(48,237,193,1)]">
+        <nav className="flex flex-col md:flex-row
+        justify-between items-center md:items-end
+        bg-background p-10  inter">
+            <div className="text-2xl md:text-3xl
+                font-light tracking-widest text-transparent [-webkit-text-stroke:1px_rgb(48,237,193,1)]
+                mb-4 md:mb-0 ">
                 <Link href="/">COMPowser</Link>
             </div>
-            <div className="flex text-[#777EA8] gap-10 text-xl">
-            <Link href="/components.js">COMPONENTS</Link>
-            <Link href="/github.js">GITHUB</Link>
-            <Link href="/about_us.js">ABOUT <span className="text-[#30EDC1]">US</span></Link>
+            <div className="flex flex-col sm:flex-row
+                gap-4 sm:gap-10 text-lg md:text-xl text-(--cardColor)">
+                <Link href="/components.js">COMPONENTS</Link>
+                <Link href="/github.js">GITHUB</Link>
+                <Link href="/about_us.js">ABOUT <span className="text-(--logoColor)">US</span></Link>
             </div>
         </nav>
     )
 }
+
+ 
