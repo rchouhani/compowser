2d7fe6f1de82db9b5f1b6174d204b267b59d9e84
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-02-05 22:21:29 +0100

    create: footer

---
+++ compowser/src/app/components/Footer.js
@@ -0,0 +1,21 @@
+import { Copyright } from "lucide-react";
+import Link from "next/link";
+
+export default function Footer(){
+    return(
+        <footer className="flex flex-col gap-5 p-5 mt-6 border-t-2 border-t-(--border)">
+            <div className="text-sm md:text-2xl
+                font-light tracking-widest text-transparent [-webkit-text-stroke:1px_rgb(48,237,193,1)]
+                mb-4 md:mb-0 ">
+                <Link href="/">COMPowser</Link>
+            </div>
+           
+            <p className="flex flex-row items-center justify-center gap-2"> Tous droits réservés à 
+                <a href="https://www.linkedin.com/in/camille-lebigot-pro/" className="font-bold text-(--logoColor)">Camille</a>|
+                <a href="www.linkedin.com/in/fdiarra" className="font-bold text-(--logoColor)">Faty</a>|
+                <a href="https://www.linkedin.com/in/romain-chouhani-334b1586/" className="font-bold text-(--logoColor)">Romain</a>
+            </p>
+
+        </footer>
+    )
+}
