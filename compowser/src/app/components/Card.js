10276ec7150ba39c91d7018bdf0cf657360bd354
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-02-05 19:48:13 +0100

    create: card component qui englobe chaque component

---
+++ compowser/src/app/components/Card.js
@@ -0,0 +1,14 @@
+export default function Card (props){
+    return(
+        <article className="border-2 rounded-2xl  w-130 border-(--borderCard)">
+            <h2 className="uppercase font-black pb-3 text-blue-400 p-5"> {props.title} </h2>
+                <div className="mb-5 h-10 m-2">
+                    {props.component}
+                </div>
+   
+             <div className="bg-(--borderCard) rounded-b-2xl p-2 mt-10">
+                    {props.code}
+                </div>
+        </article>
+    )
+}
