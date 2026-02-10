32296236a75729bb95d6879e0d3ce7d0a9364dff
Camille-lt <camillelebigot5@gmail.com>
2026-02-10 10:38:55 +0100

    updated navbar delete inter

--- compowser/src/app/components/Navbar.js
+++ compowser/src/app/components/Navbar.js
@@ -4,7 +4,7 @@ export default function () {
     return (
         <nav className="flex flex-col md:flex-row
         justify-between items-center md:items-end
-        bg-background p-10  inter">
+        bg-background p-10 ">
             <div className="text-2xl md:text-3xl
                 font-light tracking-widest text-transparent [-webkit-text-stroke:1px_rgb(48,237,193,1)]
                 mb-4 md:mb-0 ">
