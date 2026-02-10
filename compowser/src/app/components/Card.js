88ca7fb4e3dcbd4a38757d6c35e45f5a53eb2cc5
Camille-lt <camillelebigot5@gmail.com>
2026-02-10 15:26:53 +0100

    updated size of cards components

--- compowser/src/app/components/Card.js
+++ compowser/src/app/components/Card.js
@@ -1,12 +1,12 @@
-export default function Card (props){
-    return(
+export default function Card(props) {
+    return (
         <article className="border-2 rounded-2xl  w-130 border-(--borderCard)">
             <h2 className="uppercase font-black pb-3 p-5"> {props.title} </h2>
-                <div className="mb-5 h-10 m-2">
-                    {props.component}
-                </div>
-   
-           <div className="bg-(--borderCard) rounded-b-2xl p-2 mt-10">
+            <div className="mb-5 min-h-fit m-2">
+                {props.component}
+            </div>
+
+            <div className="bg-(--borderCard) rounded-b-2xl p-2 mt-10">
                 <span className="cursor-pointer transition-all hover:scale-110 hover:opacity-70 inline-block">
                     {props.code}
                 </span>
