4785bac527a9847446d1e4c45ee03f2ddd0c3b58
Camille-lt <camillelebigot5@gmail.com>
2026-02-10 11:07:59 +0100

    updated Card : delete the color of the title h2

--- compowser/src/app/components/Card.js
+++ compowser/src/app/components/Card.js
@@ -1,7 +1,7 @@
 export default function Card (props){
     return(
         <article className="border-2 rounded-2xl  w-130 border-(--borderCard)">
-            <h2 className="uppercase font-black pb-3 text-blue-400 p-5"> {props.title} </h2>
+            <h2 className="uppercase font-black pb-3 p-5"> {props.title} </h2>
                 <div className="mb-5 h-10 m-2">
                     {props.component}
                 </div>
