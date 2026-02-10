ae5c302d7394a3ce8a4861f0cd9db5c29d2fc014
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-02-10 15:30:02 +0100

    edit: card height to fit all components

--- compowser/src/app/components/Card.js
+++ compowser/src/app/components/Card.js
@@ -1,16 +1,21 @@
-export default function Card(props) {
-    return (
-        <article className="border-2 rounded-2xl  w-130 border-(--borderCard)">
-            <h2 className="uppercase font-black pb-3 p-5"> {props.title} </h2>
-            <div className="mb-5 min-h-fit m-2">
-                {props.component}
-            </div>
+export default function Card (props) {
+  return (
+    <article className="border-2 rounded-2xl w-130 border-[var(--borderCard)] flex flex-col">
+      
+      <h2 className="uppercase font-black pb-3 p-5">
+        {props.title}
+      </h2>
 
-            <div className="bg-(--borderCard) rounded-b-2xl p-2 mt-10">
-                <span className="cursor-pointer transition-all hover:scale-110 hover:opacity-70 inline-block">
-                    {props.code}
-                </span>
-            </div>
-        </article>
-    )
+      <div className="mb-5 m-2 flex-grow">
+        {props.component}
+      </div>
+
+      <div className="bg-[var(--borderCard)] rounded-b-2xl p-2">
+        <span className="cursor-pointer transition-all hover:scale-110 hover:opacity-70 inline-block">
+          {props.code}
+        </span>
+      </div>
+
+    </article>
+  );
 }
