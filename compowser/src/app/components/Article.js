ba0cd263d8845baca8862011a829794be28cbaea
Camille-lt <camillelebigot5@gmail.com>
2026-02-12 14:43:23 +0100

    updated picture

--- compowser/src/app/components/Article.js
+++ compowser/src/app/components/Article.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Article({ image, name, text }) {
   return (
     <article className="border-2 rounded-2xl w-full md:w-[30%] border-(--borderCard) flex flex-col overflow-hidden h-full md:h-[600px]">
-      <div className="relative h-64 md:h-80 w-full flex-shrink-0">
+      <div className="relative h-64 md:h-80 w-full shrink-0">
         <Image
           src={image}
           alt={name}
@@ -12,11 +12,11 @@ export default function Article({ image, name, text }) {
         />
       </div>
 
-      <div className="flex flex-col flex-grow">
+      <div className="flex flex-col grow">
         <h2 className="uppercase font-black py-3 px-5 text-(--logoColor) text-center">
           {name}
         </h2>
-        <p className="px-5 pb-5 text-justify text-(--textColor) flex-grow">
+        <p className="px-5 pb-5 text-justify text-(--textColor) grow">
           {text}
         </p>
       </div>
