fd66b501baa4c3ece3496141813d781797e0b0fd
rchouhani <rchouhani@me.com>
2026-02-17 15:50:24 +0100

    add/ CardCard code

--- compowser/src/app/data/componentsData.js
+++ compowser/src/app/data/componentsData.js
@@ -154,5 +154,20 @@ CardFooter: `<footer className="flex flex-col items-start text-left w-full bg-(-
         <Link className="hover:underline hover:font-bold" href="#">GitHub</Link>
         <Link className="hover:underline hover:font-bold" href="#">À propos</Link>
       </div>
-    </footer>`
+    </footer>`,
+
+    CardCard: `<div className="border-solid border-2 border-[#30EDC1] p-3 rounded-lg">
+          <div className="flex items-center justify-center w-full flex-shrink-0">
+            <Image src="/favicon-16x16.png" alt="image" width={100} height={100} />
+          </div>
+    
+          <div className="flex flex-col flex-grow">
+            <h2 className="uppercase font-black py-3 px-5 text-(--logoColor) text-center">
+              Titre
+            </h2>
+            <p className="px-5 pb-5 text-center text-(--textColor) flex-grow">
+              Ceci est un exemple de carte pour afficher ce que vous voulez.
+            </p>
+          </div>
+        </div>`
 }
