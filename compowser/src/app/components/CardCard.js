5bb30158fa8cb2fd8e6bdaca59b3029dc6d40f5c
rchouhani <rchouhani@me.com>
2026-02-17 15:52:07 +0100

    add/ border ro view border

--- compowser/src/app/components/CardCard.js
+++ compowser/src/app/components/CardCard.js
@@ -2,19 +2,19 @@ import Image from "next/image";
 
 export default function CardCard() {
   return (
-    <>
-      <div className="relative center h-40 md:h-80 w-full flex-shrink-0">
-        <Image src="/favicon-16x16.png" alt="image" fill className="object-cover" />
+    <div className="border-solid border-2 border-[#30EDC1] p-3 rounded-lg">
+      <div className="flex items-center justify-center w-full flex-shrink-0">
+        <Image src="/penguin.png" alt="image" width={100} height={100} />
       </div>
 
       <div className="flex flex-col flex-grow">
         <h2 className="uppercase font-black py-3 px-5 text-(--logoColor) text-center">
-        Titre  
+          Titre
         </h2>
-        <p className="px-5 pb-5 text-justify text-(--textColor) flex-grow">
+        <p className="px-5 pb-5 text-center text-(--textColor) flex-grow">
           Ceci est un exemple de carte pour afficher ce que vous voulez.
         </p>
       </div>
-    </>
+    </div>
   );
 }
